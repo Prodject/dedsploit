b1f8b0671223495308676ffc553788ae4c6a9cab
ex0dus-0x <ex0dus@codemuch.tech>
2019-05-01 07:07:55 -0400

    TODO
    * cleanup code
    * update to py3
    * python module structure

--- dedsploit/__main__.py
+++ dedsploit/__main__.py
@@ -1,32 +1,29 @@
-#!/usr/bin/env python
-from core import *
-from modules import http, net, smtp
+#!/usr/bin/env python3
+import os
+import sys
+import platform
 
-# Ensure that the user has root privileges!
-if not os.geteuid() == 0:
-    sys.exit(R + "[!] You are not root! [!]" + W )
+import dedsploit.core as core
+from dedsploit.modules import http, net, smtp
 
-# Recommendation platform: Linux
-if platform.system() != "Linux":
-    print R + "[!] You are not using Linux! dedsploit may not work! [!]" + W
-
-# Available modules in the form of a dict
+# Available modules
 modules = {
-    "http": ["slowloris"],
-    "net": ["arpspoof", "pscan", "hosts"],
-    "smtp": ["smsbomb"]
+    "http":     ["slowloris"],
+    "net":      ["arpspoof", "pscan", "hosts"],
+    "smtp":     ["smsbomb"]
 }
 
-# Display visually appealing output!
 
+# Display visually appealing output!
 header = C + """
     .___         .___             .__         .__  __
   __| _/____   __| _/____________ |  |   ____ |__|/  |_
  / __ |/ __ \ / __ |/  ___/\____ \|  |  /  _ \|  \   __|
 / /_/ \  ___// /_/ |\___ \ |  |_> >  |_(  <_> )  ||  |
 \____ |\___  >____ /____  >|   __/|____/\____/|__||__|
-     \/    \/     \/    \/ |__|                         
-http://dedsploit.tech\n""" + W
+     \/    \/     \/    \/ |__|
+\n""" + W
+
 
 # Print authors, versions and available modules/attack vectors
 version = LG + """\n
@@ -34,36 +31,48 @@ version = LG + """\n
          [   Version: 3.0.0                     ]
          [   Modules: %s                         ]\n""" % len(modules) + W
 
-# Output info based on network
-netinfo = """
-		> Local IP Address: %s                > Gateway IP: %s
-		> MAC Address: %s
-		""" % (str(lan_ip), str(gateway_ip), str(mac_address))
 
-print header, version, netinfo
+def print_command_help(command_help):
+    """
+    unpacks each list, and create a help menu
+    """
+    help_map = dict(command_help)
+    for h in help_map:
+        print("{0}\t\t\t{1}".format(h, help_map[h]) + W)
 
-# Returns values from each sublist within a dict
 
 def get_key(value):
+    """
+    returns values from each sublist within a dict
+    """
     return next(key for key, lst in modules.iteritems() for item in lst if item == value)
 
-# Main entry point
 
 def main():
-    print LC + "Type in a command. For system commands, type 'help'.\nFor available modules, type 'modules'. Exit with Ctrl + C or 'exit'.\n"
+
+    # check platform
+    if platform.system() != "Linux":
+        print(R + "[!] You are not using Linux! dedsploit may not work! [!]" + W)
+
+    # print header and info
+    print(header, version, netinfo)
+    print(LC + "Type in a command. For system commands, type 'help'.\nFor available modules, type 'modules'. Exit with Ctrl + C or 'exit'.\n")
+
+    # input loop
     while True:
         options = raw_input(LP + "[>>] " + W )
+
         if options == "help":
+            print(f"{LR}\n[Commands Available:]\n{LG}")
             print_command_help(help_options)
             continue
         elif options == "modules":
-            print LR + "\n[There are currently """ + LP + "{}".format(sum(len(lst) for lst in modules.values())) + LR + " modules available:]\n" + LG
+            print(LR + "\n[There are currently " + LP + "{}".format(sum(len(lst) for lst in modules.values())) + LR + " modules available:]\n" + LG
+
             # Iterate over each key value in the modules dict
             for protocol, module_list in modules.items():
-                # For each item in each sublist, print
                 for mod in module_list:
-                    print "{}\t\t({})".format(str(mod), str(protocol))        
-            print "\n"
+                    print("{}\t\t({})\n".format(str(mod), str(protocol)))
             continue
         elif options == "clear":
             os.system("clear")
@@ -71,22 +80,24 @@ def main():
         elif options == "exit":
             raise EOFError
         elif options.startswith("use"):
-            modname = options.split()[1]
+
             # return a list of all concatenated sublists
+            modname = options.split()[1]
             mod_list = [name for lst in modules.values() for name in lst]
+
             if modname in mod_list:
-                print LISTMSG
+                print(LISTMSG)
                 protocol = get_key(modname)
                 if protocol == "http":
-                    h = http.HTTP(modname).execute()
+                    http.HTTP(modname).execute()
                 elif protocol == "net":
-                    n = net.Net(modname).execute()
+                    net.Net(modname).execute()
                 elif protocol == "smtp":
-                    s = smtp.SMTP(modname).execute()
+                    smtp.SMTP(modname).execute()
             else:
-                print WARNING
+                print(WARNING)
         else:
-            print WARNING
+            print(WARNING)
             continue
 
 if __name__ == "__main__":
